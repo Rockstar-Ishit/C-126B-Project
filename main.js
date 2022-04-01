0f06c0784292b4138f3a140fdeceb7d33e77169a
Rockstar-Ishit <74503799+Rockstar-Ishit@users.noreply.github.com>
2022-04-01 12:44:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+rightWristY = 0;
+leftWristY = 0;
+rightwristX = 0;
 
 function preload() {
     song1 = loadSound("music.mp3");
@@ -11,8 +15,23 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded() {
+    console.log('poseNet is Initialized');
+}
+
+function gotPoses(results) {
+  if(results.length > 0) {
+    console.log(results);
+    leftwristX=results[0].pose.leftWrist.x; rightwristX=results[0].pose.rightWrist.x;
+     difference=floor(leftwristX-rightwristX);
+     console.log("leftwristX = " + leftwristX + "rightwristX = " + rightwristX + " difference = " + difference);
+  }
 }
